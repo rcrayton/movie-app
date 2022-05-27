a9e3aa3f32ee2b5fef97451c8059b5eb0a779cb4
Rikki Crayton <rikkicrayton@Rikkis-Air.attlocal.net>
2022-05-27 17:31:58 -0400

    Ensured API pulls movies name

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import { useEffect } from 'react';
 
 const API_URL = 'http://www.omdbapi.com?apikey=1dc19b08';
 
-const Apps= ()=>{
+const App = ()=>{
   return(
+
+
     const searchMovies = async (title) => {
       const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response .json();
+
+      console.log(data.search);
     }
 
     useEffect(()=> {
-
+      searchMovies('spiderman')
     }, [])
     <h1>App</h1>
   );
