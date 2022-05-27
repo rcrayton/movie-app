a35495713899d69d1974fc1ba8d94d99755705fa
Rikki Crayton <rikkicrayton@Rikkis-Air.attlocal.net>
2022-05-27 16:53:10 -0400

    Added API call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const API_URL = 'http://www.omdbapi.com?apikey=1dc19b08';
+
+const Apps= ()=>{
+  return(
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`);
+    }
+
+    useEffect(()=> {
+
+    }, [])
+    <h1>App</h1>
   );
 }
 
