558cbdc7af5b524a76b80a05e99a9c10956e02f0
Rikki Crayton <rikkicrayton@Rikkis-Air.attlocal.net>
2022-05-27 17:45:18 -0400

    Imported stylesheet and search button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { useEffect } from 'react';
 // import logo from './logo.svg';
-// import './App.css';
-
+import './App.css';
+import SearchIcon from './search.svg';
 const API_URL = 'http://www.omdbapi.com?apikey=1dc19b08';
 
 const App = ()=>{
+
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response .json();
+
+    console.log(data.search);
+  }
+
+  useEffect(()=> {
+    searchMovies('spiderman')
+  }, [])
   return(
-
-
-    const searchMovies = async (title) => {
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response .json();
-
-      console.log(data.search);
-    }
-
-    useEffect(()=> {
-      searchMovies('spiderman')
-    }, [])
     <h1>App</h1>
   );
 }
