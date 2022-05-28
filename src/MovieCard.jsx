373c5e0ea9ac5e8559fcfbe9daf841003a46366d
Rikki Crayton <rikkicrayton@Rikkis-MacBook-Air.local>
2022-05-28 11:46:13 -0400

    Destructured object for movie card

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 
-const MovieCard = ({movie2}) =>{
+const MovieCard = ({movie}) =>{
   return(
     <div className='movie'>
     <div>
-      <p>{movie2.Year}</p>
+      <p>{movie.Year}</p>
     </div>
 
     <div>
-      <img src={movie2.Poster !== 'N/A' ? movie2.Poster : 'http://via.placeholder.com/400'} alt={movie2.Title}/>
+      <img src={movie.Poster !== 'N/A' ? movie.Poster : 'http://via.placeholder.com/400'} alt={movie.Title}/>
     </div>
     <div>
-      <span>{movie2.Type}</span>
-      <h3>{movie2.Title}</h3>
+      <span>{movie.Type}</span>
+      <h3>{movie.Title}</h3>
     </div>
   </div>
   )
